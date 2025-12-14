c0757170d168309655e118c0e29b9b91ab8e2192
Jaya Banala <coconutjellytarot@gmail.com>
2025-12-13 18:52:45 -0800

    Fixing backend team build and stop scripts, docker container updates, front end dev is now started with back team dev env

--- team_dev_environments/backend-team/build.sh
+++ team_dev_environments/backend-team/build.sh
@@ -16,8 +16,7 @@ SERVICES=(
   api-gateway auth-service task-orchestrator
   engagement-service platform-analytics-service
   notification-service external-bridge-service
-  challenge-service realtime-gateway
-  synapse
+  challenge-service realtime-gateway synapse frontend-dev 
 )
 
 echo "🔨 Building Backend Team services..."
