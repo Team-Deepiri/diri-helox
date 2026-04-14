f68653ec79e031ccfe8ed638a84b9c1f867fe452
Mahlaka <samimahlaka@gmail.com>
2026-04-13 19:13:05 -0500

    Fix CI imports and dataset versioning compatibility

--- utils/semantic_analyzer.py
+++ utils/semantic_analyzer.py
@@ -1,5 +1,5 @@
 try:
-    from deepiri_modelkit.ml.semantic import SemanticAnalyzer, get_semantic_analyzer
+    from deepiri_modelkit.ml.semantic import get_semantic_analyzer
 except ImportError:
-    SemanticAnalyzer = None
+    
     get_semantic_analyzer = None
