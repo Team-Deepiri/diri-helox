3797f0f2b9fce5702efbf2f9ff48649027b8df79
Patrick Roland Wijaya <pwijaya@cs.washington.edu>
2026-04-27 11:59:45 -0700

    export gpu helper functions from package root

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,14 @@
 """Core utilities for LLM training infrastructure."""
 
 from .device_manager import DeviceManager, get_optimal_device
+from .gpu_utils import detect_device, get_gpu_info, is_gpu_available
 from .training_config import TrainingConfig, ModelConfig, DataConfig
 
 __all__ = [
     "DeviceManager",
+    "detect_device",
+    "get_gpu_info",
+    "is_gpu_available",
     "get_optimal_device",
     "TrainingConfig",
     "ModelConfig",
