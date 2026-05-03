90b95aa849268771e2a101408253607dd574a158
Patrick Roland Wijaya <pwijaya@cs.washington.edu>
2026-05-02 23:20:40 -0700

    feat(sdk): ship deepiri-helox-sdk under helox_sdk for Mudspeed consumers

--- core/__init__.py
+++ core/__init__.py
@@ -2,6 +2,7 @@
 
 from .device_manager import DeviceManager, get_optimal_device
 from .gpu_utils import detect_device, get_gpu_info, is_gpu_available
+from .mudspeed_gpu import resolve_mudspeed_torch_device
 from .training_config import TrainingConfig, ModelConfig, DataConfig
 
 __all__ = [
@@ -10,6 +11,7 @@ __all__ = [
     "get_gpu_info",
     "is_gpu_available",
     "get_optimal_device",
+    "resolve_mudspeed_torch_device",
     "TrainingConfig",
     "ModelConfig",
     "DataConfig",
