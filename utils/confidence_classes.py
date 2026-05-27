fb889cc8c0cd1db265665a5f8eecf01046d745d3
Devin <devgamble526@gmail.com>
2026-05-27 17:41:03 -0400

    confidence symbols from deepiri_modelkit.ml.confidence

--- utils/confidence_classes.py
+++ utils/confidence_classes.py
@@ -1,7 +1,17 @@
 try:
-    from deepiri_modelkit.ml.confidence import ConfidenceLevel
+    from deepiri_modelkit.ml.confidence import (
+        ConfidenceAttributes,
+        ConfidenceCalculator,
+        ConfidenceLevel,
+        ConfidenceSource,
+        get_confidence_calculator,
+    )
 except ImportError:
+    ConfidenceAttributes = None
+    ConfidenceCalculator = None
     ConfidenceLevel = None
+    ConfidenceSource = None
+    get_confidence_calculator = None
 
 
 __all__ = [
